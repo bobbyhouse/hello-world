97344f9577353fb30a023f50a530042f33146975
Bobby House <robert.house@docker.com>
2025-01-27 14:45:56 -0800

    make updates

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 // Create the server
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello, Node.js with Docker!');
+  res.end('Hello, Node.js with Dockerss!');
 });
 
 // Start the server
